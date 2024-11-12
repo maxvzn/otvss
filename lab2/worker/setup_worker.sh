7db7921c188c3bb37d27fd0083afac26155a274f
maxvzn <maksimvaznik@gmail.com>
2024-11-12 21:39:04 +0300

    fix setup_worker

--- lab2/worker/setup_worker.sh
+++ lab2/worker/setup_worker.sh
@@ -22,11 +22,8 @@ EOF
 
 sudo systemctl restart docker
 
-# Установка Kubernetes
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
